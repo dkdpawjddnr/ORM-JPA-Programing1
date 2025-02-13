dabc69f1d3066dc47fd62094a0210c2284f4f466
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-02-13 19:28:32 +0900

    단방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,19 +16,24 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // 저장
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeamId(team.getId());
-            em.persist(member);
+            member.setTeam(team);
 
             Member findMember = em.find(Member.class, member.getId());
 
-            Long findTeamId = findMember.getTeamId();
-            Team findTeam = em.find(Team.class, findTeamId);
+            Team findTeam = findMember.getTeam();
+            System.out.println("finaTeam = " + findTeam.getName());
+
+            // 연관관계 수정
+            Team newTeam = em.find(Team.class, 100L);
+            // 외래키 값이 업데이트 된다.
+            findMember.setTeam(newTeam);
 
             tx.commit();
         } catch (Exception e){
