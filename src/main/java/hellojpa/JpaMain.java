a108201dcf7f76c330b7479372dc9e1207460831
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-03-07 23:26:48 +0900

    즉시 로딩 EAGER 는 N+1문제를 야기함.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -17,22 +19,31 @@ public class JpaMain {
             team.setName("teamA");
             em.persist(team);
 
+            Team teamB = new Team();
+            team.setName("teamB");
+            em.persist(teamB);
+
             Member member1 = new Member();
             member1.setUsername("member1");
             member1.setTeam(team);
             em.persist(member1);
 
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
+
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
-
-            System.out.println("m = " + m.getTeam().getClass());
-
-            System.out.println("==========");
-            //LAZY getName() 프록시 초기화, 실제 team을 사용하는 시점에 초기화
-            System.out.println("teamName = " + m.getTeam().getName());
-            System.out.println("==========");
+//            Member m = em.find(Member.class, member1.getId());
+            // EAGER 즉시 로딩은 JPQL에서 N+1 문제를 일으킨다.
+            // Member를 가지고 왔더니 Team이 즉시로딩이 되어있음. 값이 다 나와 있어야 함.
+            // Member쿼리 나가고 쿼리가 별도로 나가게 됨.
+            List<Member> members = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+            // SQL: select * from Member;
+            // SQL: select % from Team where TEAM_ID = xxx
 
             tx.commit();
         } catch (Exception e){
