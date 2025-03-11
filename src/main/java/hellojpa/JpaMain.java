b54b2e1779d70afac651c53044cb105e98a6fa7c
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-03-11 22:37:46 +0900

    값 타입 복사

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,12 +20,13 @@ public class JpaMain {
             member1.setHomeAddress(address);
             em.persist(member1);
 
+            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
             Member member2 = new Member();
             member2.setUsername("member2");
-            member2.setHomeAddress(address);
+            member2.setHomeAddress(copyAddress);
             em.persist(member2);
 
-            //부작용 (side effect)
             member1.getHomeAddress().setCity("newCity");
 
             tx.commit();
