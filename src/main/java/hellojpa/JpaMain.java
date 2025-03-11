c4171815a951da8b30aa37b47fac2cca72f09edc
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-03-11 22:24:59 +0900

    값 타입 side effect 발생

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,12 +13,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "10"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city","street", "10");
 
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setHomeAddress(address);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setHomeAddress(address);
+            em.persist(member2);
+
+            //부작용 (side effect)
+            member1.getHomeAddress().setCity("newCity");
 
             tx.commit();
         } catch (Exception e){
