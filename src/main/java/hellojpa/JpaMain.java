f8d73578a0d2f30e457eaa46e272b207bec892de
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-03-11 23:26:24 +0900

    값 타입의 컬렉션 사용

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,9 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+import java.util.Set;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -13,18 +16,38 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address = new Address("city","street", "10");
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setHomeAddress(address);
-            em.persist(member1);
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity1", "street", "10000"));
 
-            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
-            member1.setHomeAddress(newAddress);
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("곱창");
 
-            String name = "안ㅇ";
-            name = "SDKFSFD";
+            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+
+            // 값타입들은 따로 persist 안해줘도 됨.
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("=======Start=======");
+            Member findMember = em.find(Member.class, member.getId());
+
+            // 값 타입 컬렉션들은 다 지연로딩
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for(Address address : addressHistory){
+                System.out.println("address = " + address.getCity());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for(String favoriteFood : favoriteFoods){
+                System.out.println("favoritFood = " + favoriteFood);
+            }
+
 
             tx.commit();
         } catch (Exception e){
