840e9d437c420cc44284c6c531f5bf208f017871
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-03-11 23:01:18 +0900

    값 타입의 비교

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,14 +20,11 @@ public class JpaMain {
             member1.setHomeAddress(address);
             em.persist(member1);
 
-            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
+            member1.setHomeAddress(newAddress);
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setHomeAddress(copyAddress);
-            em.persist(member2);
-
-            member1.getHomeAddress().setCity("newCity");
+            String name = "안ㅇ";
+            name = "SDKFSFD";
 
             tx.commit();
         } catch (Exception e){
--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -3,10 +3,18 @@ package hellojpa;
 public class ValueMain {
     public static void main(String[] args) {
 
-        Integer a = Integer.valueOf(10);
-        Integer b = a;
+        int a = 10;
+        int b = 10;
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("a == b: " + (a == b));
+
+        Address address1 = new Address("city","street", "10");
+        Address address2 = new Address("city","street", "10");
+
+        // 동일성 비교 인스턴스의 참조 값을 비교 == 사용
+        System.out.println("address1 == address2: " + (address1 == address2));
+
+        // 동등성 비교 인스턴스의 값을 비교, 오버라이딩해서 equals()사용
+        System.out.println("address1 equals address2: " + (address1.equals(address2)));
     }
 }
