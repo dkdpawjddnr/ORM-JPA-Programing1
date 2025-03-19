7d63d51078f0a7c45e81d27edabe546736c7929c
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-03-19 23:21:55 +0900

    JPQL 검색

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,7 +3,6 @@ package hellojpa;
 import jakarta.persistence.*;
 
 import java.util.List;
-import java.util.Set;
 
 public class JpaMain {
 
@@ -17,38 +16,16 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity1", "street", "10000"));
+            List<Member> reulst = em.createQuery(
+                    // 테이블이 아닌 Member Entity 대상으로 query를 날림
+                    // select m 은 Member m 자체를 조회
+                    "select m from Member m where m.username like '%kim%'",
+                    Member.class
+            ).getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            // 값타입들은 따로 persist 안해줘도 됨.
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("=======Start=======");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homeCity -> newCity
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity",a.getStreet(), a.getZipcode()));
-
-            //치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            //old1 -> newCity
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
-
+            for (Member member : reulst){
+                System.out.println(member);
+            }
             tx.commit();
         } catch (Exception e){
             tx.rollback();
